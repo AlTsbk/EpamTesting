322d59f1b69fe7cf50924fc8c61c3631d9f85189
Alexey Tsyabuk <tsyabuk.alexey@gmail.com>
2019-11-19 01:27:48 +0300

    PageObject

---
+++ PageObject/PageObject/PageObject/MainPage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace PageObject.PageObject
+{
+    class MainPage
+    {
+        private readonly IWebDriver driver;
+
+        public IWebElement countryInput => GetElement("//*[@id='40-11']");
+        public IWebElement cityInput => GetElement("//*[@id='1475-PARISPU']");
+        public IWebElement placeInput => GetElement("//*[@id='2091']");
+        public IWebElement ageCheckBox => GetElement("//*[@for='chk-age']");
+        public IWebElement submitBtn => GetElement("//*[@name='btn-submit']");
+        public string errorMessage => GetText("//*[@class='ui-warn-flag-title']");
+
+        public MainPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public string GetText(string xPath)
+        {
+            WaitForElementToAppear(driver, 15, By.XPath(xPath));
+
+            return driver.FindElement(By.XPath(xPath)).Text;
+        }
+
+        public static IWebElement WaitForElementToAppear(IWebDriver driver, int waitTime, By waitingElement)
+        {
+            IWebElement wait = new WebDriverWait(driver, TimeSpan.FromSeconds(waitTime)).Until(ExpectedConditions.ElementExists(waitingElement));
+            return wait;
+        }
+
+        public IWebElement GetElement(string xPath)
+        {
+            WaitForElementToAppear(driver, 15, By.XPath(xPath));
+
+            return driver.FindElement(By.XPath(xPath));
+        }
+
+    }
+}
---
+++ PageObject/PageObject/Tests/AutoeuropeTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PageObject.PageObject;
+
+namespace PageObject.Tests
+{
+    [TestFixture]
+    class AutoeuropeTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowserChrome()
+        {
+            driver = new ChromeDriver
+            {
+                Url = "https://www.autoeurope.ru/"
+            };
+        }
+
+        [Test]
+        public void CheckAge()
+        {
+            var mainPage = new MainPage(driver);
+
+            mainPage.countryInput.Click();
+            mainPage.cityInput.Click();
+            mainPage.placeInput.Click();
+            mainPage.ageCheckBox.Click();
+            mainPage.submitBtn.Click();
+            Assert.AreEqual(mainPage.errorMessage, "Пожалуйста, укажите возраст водителя.");
+        }
+
+        [Test]
+        public void rentWithoutPlace()
+        {
+            var mainPage = new MainPage(driver);
+
+            mainPage.countryInput.Click();
+            mainPage.cityInput.Click();
+            mainPage.submitBtn.Click();
+            Assert.AreEqual(mainPage.errorMessage, "НЕ ОСТАВЛЯЙТЕ ПОЛЯ ДЛЯ ЗАПОЛНЕНИЯ ПУСТЫМИ");
+        }
+
+        [TearDown]
+        public void QuitDriver()
+        {
+            driver.Quit();
+        }
+    }
+}
